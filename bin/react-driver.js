1fbd794e692f6501441981d101a69a64884c734c
Oliver Woodings <oliver.woodings@gmail.com>
2015-12-09 13:55:57 +0000

    Add API, get code into a state where you can actually launch and run tests in browsers

--- bin/react-driver.js
+++ bin/react-driver.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
 
 process.on('unhandledRejection', function (reason) {
-  process.emit("SIGTERM")
+  process.emit('SIGTERM')
   throw reason
 })
+
+var config
+try {
+  config = require(process.cwd() + '/react-driver.conf.js')
+} catch (e) { }
 
 require('babel-register')
 require('babel-polyfill')
 
 var reactDriver = require('../lib/cli/reactDriver')
-reactDriver({})
+reactDriver(config)
